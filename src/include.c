5fc9e4794d53a1e1b90998c5e2f7e61edd784318
xskf-4 <sebastianhdezreyes05@gmail.com>
2026-05-15 22:26:21 -0600

    Añadir comentario cabecera al archivo include.c

--- src/include.c
+++ src/include.c
@@ -1,3 +1,9 @@
+/**
+ * @file include.c
+ * @brief Auxiliary file to include source files
+ * @author Hernandez Reyes Sebastian (xskf-4)
+ * @date 2026
+*/
 #define MDP_IMPLEMENTATION
 #define UTILS_IMPLEMENTATION
 #include "utils.c"
